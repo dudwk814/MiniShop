d65086f707f2b93aab5ab6114cbe9697cb16aafd
dudwk814 <kj99658103@gmail.com>
2020-10-03 21:09:48 +0900

    2020-10-03 User Service Test

---
+++ src/main/java/service/UserServiceImpl.java
@@ -0,0 +1,56 @@
+package service;
+
+import domain.UserVO;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+import mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Log4j
+public class UserServiceImpl implements UserService{
+
+    @Setter(onMethod_ = {@Autowired})
+    private UserMapper userMapper;
+
+    @Override
+    public void register(UserVO vo) {
+
+        log.info("Register : " + vo);
+
+        userMapper.register(vo);
+
+    }
+
+    @Override
+    public boolean removeUser(Long mno) {
+
+        log.info("Remove User : " + mno);
+        return userMapper.delete(mno) == 1;
+    }
+
+    @Override
+    public boolean modifyUser(UserVO vo) {
+
+        log.info("Modified User : " + vo);
+
+        return userMapper.update(vo) == 1;
+    }
+
+    @Override
+    public UserVO getUser(Long mno) {
+
+        log.info("get User Info : " + mno);
+        return userMapper.get(mno);
+    }
+
+    @Override
+    public List<UserVO> getUserList() {
+
+        log.info("get All Users");
+        return userMapper.getList();
+    }
+}
---
+++ src/test/java/service/UserServiceTests.java
@@ -0,0 +1,86 @@
+package service;
+
+import domain.UserVO;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+import mapper.UserMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:web/WEB-INF/applicationContext.xml")
+@Log4j
+public class UserServiceTests {
+
+    @Setter(onMethod_ = {@Autowired})
+    private UserService userService;
+
+
+    // 단일 회원 조회 테스트
+    @Test
+    public void testGet() {
+
+        UserVO vo = userService.getUser(2L);
+
+        log.info(vo);
+    }
+
+    // 전체 회원 조회 테스트
+    @Test
+    public void testGetList() {
+
+        List<UserVO> list = userService.getUserList();
+
+        for (UserVO user : list) {
+            log.info(user);
+        }
+    }
+
+    // 회원 가입 테스트
+    @Test
+    public void testRegister() {
+
+        UserVO vo = new UserVO();
+        vo.setMId("Ralo");
+        vo.setMName("김찬호");
+        vo.setMPw("1234");
+        vo.setMPw2("1234");
+        vo.setAddress("트위치");
+
+
+        userService.register(vo);
+    }
+
+
+    // 회원 정보 수정 테스트
+    @Test
+    public void testModify() {
+
+        // 13L
+        UserVO vo = userService.getUser(13L);
+
+        vo.setMPw("8103");
+        vo.setMPw2("8103");
+        vo.setAddress("카팟");
+
+        boolean count = userService.modifyUser(vo);
+
+        log.info("MODIFIED COUNT : " + count);
+    }
+
+    // 회원 삭제 테스트
+    @Test
+    public void testRemove() {
+
+        // 13L
+        boolean count = userService.removeUser(13L);
+
+        log.info("REMOVED COUNT : " + count);
+    }
+
+}
