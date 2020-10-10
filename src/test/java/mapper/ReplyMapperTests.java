e638501cdb76e3fc421df78585bc4f6f8c39e037
dudwk814 <kj99658103@gmail.com>
2020-10-10 17:35:00 +0900

    2020-10-10 댓글 기능 mapper test

--- src/test/java/mapper/ReplyMapperTests.java
+++ src/test/java/mapper/ReplyMapperTests.java
@@ -1,5 +1,6 @@
 package mapper;
 
+import domain.ReplyVO;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 import org.junit.Test;
@@ -7,6 +8,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.stream.IntStream;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:web/WEB-INF/applicationContext.xml")
@@ -16,10 +19,55 @@ public class ReplyMapperTests {
     @Setter(onMethod_ = {@Autowired})
     private ReplyMapper mapper;
 
+    // Bno 배열
+    private Long[] bnoArr = {1038L, 1037L, 1036L, 1035L, 1034L};
+
     // mapper 파일 빈 생성 테스트
     @Test
     public void testMapper() {
 
         log.info(mapper);
+    }
+
+    // 댓글 작성 테스트
+    @Test
+    public void testCreate() {
+
+        IntStream.rangeClosed(1, 10).forEach(i -> {
+            ReplyVO vo = new ReplyVO();
+
+            vo.setBno(bnoArr[i % 5]);
+            vo.setReply("댓글 테스트" + i);
+            vo.setReplyer("replyer" + i);
+
+            mapper.insert(vo);
+        });
+    }
+
+    // 단일 댓글 조회 테스트
+    @Test
+    public void testRead() {
+
+        log.info(mapper.read(2L));
+    }
+
+    // 댓글 삭제 테스트
+    @Test
+    public void testDelete() {
+
+        log.info(mapper.delete(11L));
+    }
+
+    // 댓글 수정 테스트
+    @Test
+    public void testUpdate() {
+
+        ReplyVO vo = mapper.read(2L);
+
+        vo.setReply("댓글 수정 테스트");
+
+        int count = mapper.update(vo);
+
+        log.info("UPDATE COUNT : " + count);
     }
 }
