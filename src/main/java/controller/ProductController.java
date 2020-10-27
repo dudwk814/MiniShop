5e9a93fc1a26c24da4232c7a368d74010995c19c
dudwk814 <kj99658103@gmail.com>
2020-10-27 15:35:17 +0900

    2020-10-27

--- src/main/java/controller/ProductController.java
+++ src/main/java/controller/ProductController.java
@@ -1,18 +1,33 @@
 package controller;
 
+import domain.ProductVO;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import service.ProductService;
 
 @Controller
+@RequestMapping("/product/")
 @Log4j
 public class ProductController {
 
     @Setter(onMethod_ = @Autowired)
     private ProductService productService;
 
+    // 상품 조회
+    @GetMapping("/read")
+    public String read(int product_id, Model model) {
+
+        ProductVO product = productService.getProduct(product_id);
+
+        model.addAttribute("product", product);
+
+        return "product/productRead";
+    }
     
 
 }
---
+++ web/WEB-INF/views/product/productRead.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: PCY
+  Date: 2020-10-27
+  Time: 오후 3:33
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    ${product.product_name}
+</body>
+</html>
