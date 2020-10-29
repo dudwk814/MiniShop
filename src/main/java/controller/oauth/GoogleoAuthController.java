cd0be6ded402bc3a15681558392bdf9205526026
dudwk814 <kj99658103@gmail.com>
2020-10-30 02:15:32 +0900

    2020-10-30

--- src/main/java/controller/oauth/GoogleoAuthController.java
+++ src/main/java/controller/oauth/GoogleoAuthController.java
@@ -1,14 +1,24 @@
 package controller.oauth;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 import security.oauth.GoogleOAuthRequest;
+import security.oauth.GoogleOAuthResponse;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @Log4j
@@ -23,8 +33,15 @@ public class GoogleoAuthController {
     @Value("${api.client_secret}")
     String clientSecret;
 
+
+    /**
+     * Authentication Code를 전달 받는 엔드포인트
+     **/
     @GetMapping("/login/google/auth")
-    public String googleAuth(Model model, @RequestParam(value = "code") String authCode) throws JsonProcessingException {
+    public String googleAuth(Model model, @RequestParam(value = "code") String authCode)
+            throws JsonProcessingException {
+
+        //HTTP Request를 위한 RestTemplate
         RestTemplate restTemplate = new RestTemplate();
 
         //Google OAuth Access Token 요청을 위한 파라미터 세팅
@@ -36,6 +53,53 @@ public class GoogleoAuthController {
                 .redirectUri("http://localhost:8080/login/google/auth")
                 .grantType("authorization_code").build();
 
-        return "";
+
+        //JSON 파싱을 위한 기본값 세팅
+        //요청시 파라미터는 스네이크 케이스로 세팅되므로 Object mapper에 미리 설정해준다.
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+        //AccessToken 발급 요청
+        ResponseEntity<String> resultEntity = restTemplate.postForEntity(GOOGLE_TOKEN_BASE_URL, googleOAuthRequestParam, String.class);
+
+        //Token Request
+        GoogleOAuthResponse result = mapper.readValue(resultEntity.getBody(), new TypeReference<GoogleOAuthResponse>() {
+        });
+
+        //ID Token만 추출 (사용자의 정보는 jwt로 인코딩 되어있다)
+        String jwtToken = result.getIdToken();
+        String requestUrl = UriComponentsBuilder.fromHttpUrl("https://oauth2.googleapis.com/tokeninfo")
+                .queryParam("id_token", jwtToken).encode().toUriString();
+
+        String resultJson = restTemplate.getForObject(requestUrl, String.class);
+
+        Map<String,String> userInfo = mapper.readValue(resultJson, new TypeReference<Map<String, String>>(){});
+        model.addAllAttributes(userInfo);
+        model.addAttribute("token", result.getAccessToken());
+
+
+
+        return "/google.html";
+
+    }
+
+    /**
+     * 토큰 무효화
+     **/
+    @GetMapping("google/revoke/token")
+    public Map<String, String> revokeToken(@RequestParam(value = "token") String token) throws JsonProcessingException {
+
+        Map<String, String> result = new HashMap<>();
+        RestTemplate restTemplate = new RestTemplate();
+        final String requestUrl = UriComponentsBuilder.fromHttpUrl(GOOGLE_REVOKE_TOKEN_BASE_URL)
+                .queryParam("token", token).encode().toUriString();
+
+        String resultJson = restTemplate.postForObject(requestUrl, null, String.class);
+        result.put("result", "success");
+        result.put("resultJson", resultJson);
+
+        return result;
+
     }
 }
